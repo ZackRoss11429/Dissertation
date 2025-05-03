b0e56b19b85cc503000b87586b1a475b53dd0330
ZackRoss11429 <21011429@hope.ac.uk>
2025-05-04 00:55:16 +0100

    - finished elliptic_curves.py - started work on benchmarking based on time and space - finished baby-step-giant-step algorithm for ECC - created main.py to tie all processes together
    04/05/2025 - 1

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from utils import k_value_ec_a, k_value_ec_b
+from elliptic_curve import find_cyclic_group, is_generator, point_multiply
+from BSGS import EC_BSGS
+from benchmark import benchmark
+import random
