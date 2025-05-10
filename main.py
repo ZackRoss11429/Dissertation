0f2375fe989fd9d18c612cdccae23a8ef3e34f50
ZackRoss11429 <21011429@hope.ac.uk>
2025-05-10 16:30:36 +0100

    - Added json result export (results.json)
    10/05/2025 - 1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from utils import p
+import json
+import matplotlib.pyplot as plt
+from utils import p, priv_key_a, priv_key_b
 from elliptic_curves import ecc_parameters
 from elliptic_curves import point_multiply
 from finite_field import ff_parameters
@@ -6,56 +8,50 @@ from elliptic_curves import ecc_parameters, point_multiply
 from baby_step_giant_step import FF_BSGS, EC_BSGS
 from benchmark import benchmark
 
+all_results = {}
 
-print("Initialising Parameters:")
-ff_params = ff_parameters()
-ecc_params = ecc_parameters()
+for i in range(16):
+    print(f"Iteration: {i}")
+    print("Initialising Parameters:")
+    ff_params = ff_parameters()
+    ecc_params = ecc_parameters()
 
-FF_g = ff_params["g"]
-FF_shared_key = ff_params["A_shared"]
+    FF_g = ff_params["g"]
+    FF_shared_key = ff_params["A_shared"]
 
-EC_g = ecc_params["g"]
-EC_shared_key = ecc_params["A_shared"]
-cyclic_group = ecc_params["group"]
+    EC_g = ecc_params["g"]
+    EC_shared_key = ecc_params["A_shared"]
+    cyclic_group = ecc_params["group"]
 
-print("\nRunning Baby-Step Giant-Step Algorithm on Finite Field")
-ff_benchmark = benchmark(FF_BSGS, FF_shared_key, FF_g)
-print(f"Discrete Logarithm x = {ff_benchmark['result']}")
-print(f"Execution Time: {ff_benchmark['execution_time_sec']} sec")
-print(f"Peak Memory Usage: {ff_benchmark['peak_memory_kb']} KB")
+    print("\nRunning Baby-Step Giant-Step Algorithm on Finite Field")
+    ff_benchmark = benchmark(FF_BSGS, FF_shared_key, FF_g)
+    print(f"Discrete Logarithm x = {ff_benchmark['result']}")
+    print(f"Execution Time: {ff_benchmark['execution_time_sec']} sec")
+    print(f"Peak Memory Usage: {ff_benchmark['peak_memory_kb']} KB")
 
-print("\nRunning Baby-Step Giant-Step Algorithm on Elliptic Curve")
-ecc_benchmark = benchmark(EC_BSGS, cyclic_group, EC_g, EC_shared_key)
-print(f"Discrete Logarithm k = {ecc_benchmark['result']}")
-print(f"Execution Time: {ecc_benchmark['execution_time_sec']} sec")
-print(f"Peak Memory Usage: {ecc_benchmark['peak_memory_kb']} KB")
+    print("\nRunning Baby-Step Giant-Step Algorithm on Elliptic Curve")
+    ecc_benchmark = benchmark(EC_BSGS, cyclic_group, EC_g, EC_shared_key)
+    print(f"Discrete Logarithm k = {ecc_benchmark['result']}")
+    print(f"Execution Time: {ecc_benchmark['execution_time_sec']} sec")
+    print(f"Peak Memory Usage: {ecc_benchmark['peak_memory_kb']} KB")
 
+    results = {
+        "iteration": i,
+        "prime": p,
+        "a_private_key": priv_key_a,
+        "b_private_key": priv_key_b,
+        "ff_generator": FF_g,
+        "ff_shared": FF_shared_key,
+        "ff_exec_time (sec)": ff_benchmark['execution_time_sec'],
+        "ff_peak_memory (KB)": ff_benchmark['peak_memory_kb'],
+        "ecc_generator": EC_g,
+        "ecc_shared": EC_shared_key,
+        "ecc_exec_time (sec)": ecc_benchmark['execution_time_sec'],
+        "ecc_peak_memory (KB)": ecc_benchmark['peak_memory_kb']
+    }
 
+    all_results[f"Iteration {i}"] = results
 
+    with open('results.json', 'w') as f:
+        json.dump(all_results, f, indent=4)
 
-# print("\n ======== FINITE FIELD DIFFIE-HELLMAN =========")
-# print(f"Execution Time: {ff_result['execution_time_sec']:.6f} seconds")
-# print(f"Peak Memory: {ff_result['peak_memory_kb']:.2f} KB")
-#
-# print(f"Generator chosen: {ff['g']}")
-#
-# print(f"A's Public Key: {ff['A_public']}")
-# print(f"B's Public Key: {ff['B_public']}")
-#
-# print(f"A and B's Shared Secret Key: {ff['A_shared']}")
-#
-#
-# ecc_result = benchmark(FF_BSGS, ff_params['A_shared'], ff_params['g'])
-# ecc = ecc_result['result']
-# print("\n ======== ELLIPTIC CURVE DIFFIE-HELLMAN =========")
-# print(f"Execution Time: {ecc_result['execution_time_sec']:.6f} seconds")
-# print(f"Peak Memory: {ecc_result['peak_memory_kb']:.2f} KB")
-# print(f"Order of Cyclic Group: {ecc['order']}")
-# print(f"Generator chosen: {ecc['g']}")
-#
-# print(f"\nA's Private Key: {ecc['a']}")
-# print(f"B's Private Key: {ecc['b']}")
-#
-# print(f"\nA's Public Key: {ecc['A_public']}")
-# print(f"B's Public Key: {ecc['B_public']}")
-# print(f"Shared Secret Key of A and B: {ecc['A_shared']}")
--- utils.py
+++ utils.py
@@ -19,10 +19,11 @@ coefficient_a = 2
 coefficient_b = 2
 
 
-# private keys for person A and B -- Finite Fields
-priv_key_ff_a = 103211
-priv_key_ff_b = 195672
+# uniform private key values:
+priv_key_a = 103211
+priv_key_b = 195672
 
-# private keys for person A and B -- ECC
-k_value_ec_a = 103211
-k_value_ec_b = 195672
+# private keys for person A and B -- Finite Fields and ECC
+priv_key_ff_a = k_value_ec_a = priv_key_a
+priv_key_ff_b = k_value_ec_b = priv_key_b
+
